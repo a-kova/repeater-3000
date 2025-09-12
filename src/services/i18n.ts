9e136ff4398ab97979d0124d8dc80bbf5ee6de60
Kovalchuk Andrii <andrii.kovalchuk@justeattakeaway.com>
2025-09-12 20:45:54 +0300

    Fix

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,5 +1,10 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import i18n from 'i18n';
+
+// Recreate __filename / __dirname for ESM context
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 i18n.configure({
   locales: ['ru', 'ua'],
